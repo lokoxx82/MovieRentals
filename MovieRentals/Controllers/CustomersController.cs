9e73a7cdc3cea6f90846b3b292946b125ecb8a5e
Peter Risko <lokoxx@gmail.com>
2019-09-25 18:12:17 +0200

    read from the db

--- MovieRentals/Controllers/CustomersController.cs
+++ MovieRentals/Controllers/CustomersController.cs
@@ -5,15 +5,30 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using MovieRentals.ViewModels;
+using System.Data.Entity;
 
 namespace MovieRentals.Controllers
 {
     public class CustomersController : Controller
     {
+        //Get the DB context
+        private ApplicationDbContext _context;
+
+        public CustomersController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+        //Dispose the context object correctly
+        protected override void Dispose(bool disposing)
+        {
+            _context.Dispose();
+        }
+
         // GET: Customers
         public ActionResult Index()
         {
-            List<Customer> customers = GetCustomers();
+            List<Customer> customers = _context.Customers.Include(c=>c.MembershipType).ToList();
             CustomersViewModel viewModel = new CustomersViewModel{Customers = customers};
             return View(viewModel);
         }
@@ -23,21 +38,9 @@ namespace MovieRentals.Controllers
         //Show customer details
         public ActionResult Detail(int id)
         {
-            List<Customer> customers = GetCustomers();
-            Customer customer = customers.FirstOrDefault(x => x.Id == id);
+            Customer customer = _context.Customers.FirstOrDefault(x=>x.Id==id);
             return View(customer);
         }
 
-
-        private static List<Customer> GetCustomers()
-        {
-            List<Customer> customers = new List<Customer>
-            {
-                new Customer {Id = 1, Name = "Fero"},
-                new Customer {Id = 2, Name = "Jozo"},
-                new Customer {Id = 3, Name = "Martin"}
-            };
-            return customers;
-        }
     }
 }

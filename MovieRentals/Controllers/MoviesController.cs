8176ea54c5490fb97fbc9ba0403f5861eb31b554
Peter Risko <lokoxx@gmail.com>
2019-09-25 14:00:18 +0200

    Fundamentals

---
+++ MovieRentals/Controllers/MoviesController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MovieRentals.Models;
+using MovieRentals.ViewModels;
+
+namespace MovieRentals.Controllers
+{
+    public class MoviesController : Controller
+    {
+        // GET: Random movie
+        public ActionResult Random()
+        {
+            List<Movie> movies = new List<Movie>
+            {
+                new Movie{Id = 1,Name = "Kill bill"},
+                new Movie{Id = 2,Name = "Kill bill 2"}
+                
+            };
+
+            List<Customer> customers = new List<Customer>
+            {
+                new Customer{Id = 1, Name = "Fero"},
+                new Customer{Id = 2,Name = "Jozo"}
+            };
+
+            RandomViewModel viewModel = new RandomViewModel
+            {
+                Customers = customers,
+                Movie = movies[1]
+            };
+            return View(viewModel);
+        }
+
+        //Edit a movie
+        public ActionResult Edit(int id)
+        {
+            return Content(id.ToString());
+        }
+
+        [Route("movies/released/{year:regex(\\d{4})}/{month:regex(\\d{2}):range(1,12)}")]
+        public ActionResult ByReleaseDate(int year, int month)
+        {
+
+            return Content(year + "   " + month);
+        }
+    }
+}
---
+++ MovieRentals/ViewModels/RandomViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using MovieRentals.Models;
+
+namespace MovieRentals.ViewModels
+{
+    public class RandomViewModel
+    {
+        public Movie Movie { get; set; }
+        public IEnumerable<Customer> Customers { get; set; }
+    }
+}

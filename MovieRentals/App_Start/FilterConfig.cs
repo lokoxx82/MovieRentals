8e19acdbd939b8289d7eb358ca9f443acbf27c25
Peter Risko <lokoxx@gmail.com>
2019-09-28 21:24:36 +0200

    Change to HTTPS

--- MovieRentals/App_Start/FilterConfig.cs
+++ MovieRentals/App_Start/FilterConfig.cs
@@ -9,6 +9,7 @@ namespace MovieRentals
         {
             filters.Add(new HandleErrorAttribute());
             filters.Add(new AuthorizeAttribute());
+            filters.Add(new RequireHttpsAttribute());
         }
     }
 }

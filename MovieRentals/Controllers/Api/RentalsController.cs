5c8b15143f5d33d66fe725b3cede8b725ca27dbf
Peter Risko <lokoxx@gmail.com>
2019-09-29 15:59:28 +0200

    End to End Exercise - Add Rentals of movies

---
+++ MovieRentals/Controllers/Api/RentalsController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using MovieRentals.Dtos;
+using MovieRentals.Models;
+
+namespace MovieRentals.Controllers.Api
+{
+    public class RentalsController : ApiController
+    {
+        //Rent movies
+        //Post/Api/rentals
+        [HttpPost]
+        [Authorize(Users = RoleName.CanManageMovies)]
+        public IHttpActionResult CreateNewRentals(NewRentalDto newRentalDto)
+        {
+
+            return Created(new Uri(Request.RequestUri.ToString()), newRentalDto);
+        }
+    }
+}

a03678369969bae49f59b7236f409fadf90465a3
Peter Risko <lokoxx@gmail.com>
2019-09-29 13:11:22 +0200

    Cashing the main Index page

--- MovieRentals/Controllers/HomeController.cs
+++ MovieRentals/Controllers/HomeController.cs
@@ -3,12 +3,15 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.UI;
 
 namespace MovieRentals.Controllers
 {
     public class HomeController : Controller
     {
         [AllowAnonymous]
+        //Caching is disabled because of 0, for anabling set time and delete NoStore = true
+        [OutputCache(Duration = 0, Location = OutputCacheLocation.Server,VaryByParam = "*", NoStore = true)]
         public ActionResult Index()
         {
             return View();
